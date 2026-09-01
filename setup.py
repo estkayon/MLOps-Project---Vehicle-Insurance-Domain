a760c3529a5da11873ac0f235dd4bdef07048928
Estiak Ayon <estiakayon@gmail.com>
2026-09-01 22:41:39 +0600

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Estiak Ayon",
+    author_email="estiakayon@gmail.com",
+    packages=find_packages()
+)
