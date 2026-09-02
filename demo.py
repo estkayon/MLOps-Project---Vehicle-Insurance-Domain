dfc90749b60b9c1fd224334086f9447e8c19f444
Estiak Ayon <estiakayon@gmail.com>
2026-09-03 00:03:22 +0600

    Data Ingestion added

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 # below code is to check the logging config
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
 
 # --------------------------------------------------------------------------------
 
@@ -19,3 +19,10 @@ logging.critical("This is a critical message.")
 # except Exception as e:
 #     logging.info(e)
 #     raise MyException(e, sys) from e
+
+#----------------------------------------------------------------------------------
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
